eda66759542f13cfd23fbe732f9c1c2f84edce11
Gabriel Gregorio <gabriel@appdoconsultor.com>
2026-03-06 17:23:31 -0300

    fix: disable checkOrigin and set site URL to fix Vercel POST error

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://app-do-consultor.vercel.app',
   output: 'server',
   adapter: vercel(),
+  security: {
+    checkOrigin: false
+  },
   integrations: [
     react(),
     tailwind({
